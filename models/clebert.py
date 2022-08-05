9bacaf3e0a481c91eb1702a60665fe4f51fbdd8c
Roman Malashin <malashinroman@mail.ru>
2022-08-05 17:38:01 +0300

    fix number of classifiers + gitignore update

--- models/clebert.py
+++ models/clebert.py
@@ -10,15 +10,21 @@ class FillMask(nn.Module):
         self.device = config.device
         super(FillMask, self).__init__()
         self.bert = BertModel.from_pretrained("bert-base-uncased")
-
-        words_indexes = torch.Tensor([list(range(10))]).long().to(self.device)
+        number_of_classifiers = len(config.classifiers_indexes)
+        words_indexes = (
+            torch.Tensor([list(range(number_of_classifiers))]).long().to(self.device)
+        )
         self.bert.to(self.device)
         self.word_positional_embeddings = self.bert.embeddings.position_embeddings(
             words_indexes
         ).detach()
         self.response2embedding = nn.Linear(100, 768).to(self.device)
         self.vector2response = nn.Linear(768, 100)
-        word_type = torch.Tensor([[1 for _ in range(10)]]).long().to(self.device)
+        word_type = (
+            torch.Tensor([[1 for _ in range(number_of_classifiers)]])
+            .long()
+            .to(self.device)
+        )
         self.type_embedding = self.bert.embeddings.token_type_embeddings(
             word_type
         ).detach()
