1e28c1bd91ba28dc921982c7e094eab0b0c2523a
Roman Malashin <malashinroman@mail.ru>
2022-09-02 19:07:56 +0300

    add discrete training

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 from models.clapool import ClaPool
 from models.clebert import FillMask
 from models.clebert2 import TokenClassifier
+from models.fillmaskr_discrete import FillMaskRandDiscrete
 
 # from models.fillmaskandrandinit import FillMaskRand
 from models.fillmaskrandinit import FillMaskRand
@@ -10,6 +11,7 @@ from models.SimpleFc import SimpleFc
 
 FILLMASK = FillMask
 FILLMASK_RAND = FillMaskRand
+FILLMASK_RAND_DISCRETE = FillMaskRandDiscrete
 INDEXFILLER = IndexFiller
 TOKEN_CLASSIFIER = TokenClassifier
 SIMPLE_FC = SimpleFc
